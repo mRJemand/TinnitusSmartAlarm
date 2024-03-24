2c862dbe7fb927de646b012c364f1401262214a0
mRJemand <46520059+mRJemand@users.noreply.github.com>
2024-03-24 03:27:21 +0100

    implement settings with shared preferences

--- tinnitus_smart_alarm/lib/screens/settings_screen.dart
+++ tinnitus_smart_alarm/lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_ui/flutter_settings_ui.dart';
+import 'package:tinnitus_smart_alarm/services/settings_servics.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -9,102 +10,110 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  bool? switchValue;
-  double? volume = 0.3;
+  bool loopAlarmAudio = true;
+  bool vibrate = true;
+  bool fadeIn = true;
+  double volume = 0.3;
+  bool customVolume = true;
+
+  final SettingsService settingsService = SettingsService();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    loopAlarmAudio = await settingsService.getLoopAudioSetting();
+    vibrate = await settingsService.getVibrateSetting();
+    fadeIn = await settingsService.getFadeInSetting();
+    volume = await settingsService.getVolumeSetting();
+    customVolume = await settingsService.getCustomVolumeSetting();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: SettingsList(
-          sections: [
-            SettingsSection(
-              title: const Text('English'),
-              tiles: [
-                SettingsTile(
-                  title: const Text('English'),
-                  leading: const Icon(Icons.language),
-                  onPressed: (BuildContext context) {},
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('General'),
+            tiles: [
+              SettingsTile(
+                title: const Text('Language'),
+                leading: const Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('Alarm Settings'),
+            tiles: [
+              SettingsTile.switchTile(
+                title: const Text('Loop alarm audio'),
+                leading: const Icon(Icons.loop),
+                initialValue: loopAlarmAudio,
+                onToggle: (bool value) {
+                  setState(() {
+                    loopAlarmAudio = value;
+                  });
+                  settingsService.setLoopAudioSetting(value);
+                },
+              ),
+              SettingsTile.switchTile(
+                title: const Text('Vibrate'),
+                leading: const Icon(Icons.vibration),
+                initialValue: vibrate,
+                onToggle: (bool value) {
+                  setState(() {
+                    vibrate = value;
+                  });
+                  settingsService.setVibrateSetting(value);
+                },
+              ),
+              SettingsTile.switchTile(
+                title: const Text('Fade in'),
+                leading: const Icon(Icons.trending_up_rounded),
+                initialValue: fadeIn,
+                onToggle: (bool value) {
+                  setState(() {
+                    fadeIn = value;
+                  });
+                  settingsService.setFadeInSetting(value);
+                },
+              ),
+              SettingsTile.switchTile(
+                title: const Text('Custom volume'),
+                leading: const Icon(Icons.volume_up),
+                initialValue: customVolume,
+                onToggle: (bool value) {
+                  setState(() {
+                    customVolume = value;
+                  });
+                  settingsService.setCustomVolumeSetting(value);
+                },
+                description: Slider(
+                  value: volume,
+                  min: 0.0,
+                  max: 1.0,
+                  divisions: 100,
+                  onChanged: customVolume
+                      ? (value) {
+                          setState(() => volume = value);
+                          settingsService.setVolumeSetting(value);
+                        }
+                      : null,
                 ),
-              ],
-            ),
-            SettingsSection(
-              title: const Text('Favorite Alarm Settings'),
-              tiles: [
-                SettingsTile.switchTile(
-                  title: const Text('Loop alarm audio'),
-                  leading: const Icon(Icons.loop),
-                  initialValue: switchValue ?? true,
-                  onToggle: (bool value) {
-                    setState(() {
-                      switchValue = value;
-                    });
-                  },
-                ),
-                SettingsTile.switchTile(
-                  title: const Text('Vibrate'),
-                  leading: const Icon(Icons.vibration),
-                  initialValue: switchValue ?? true,
-                  onToggle: (bool value) {
-                    setState(() {
-                      switchValue = value;
-                    });
-                  },
-                ),
-                SettingsTile.switchTile(
-                  title: const Text('Fade in'),
-                  leading: const Icon(Icons.trending_up_rounded),
-                  initialValue: switchValue ?? true,
-                  onToggle: (bool value) {
-                    setState(() {
-                      switchValue = value;
-                    });
-                  },
-                ),
-                SettingsTile.switchTile(
-                  title: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Custom volume'),
-                      switchValue ?? false
-                          ? SizedBox(
-                              height: 30,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Icon(
-                                    volume! > 0.7
-                                        ? Icons.volume_up_rounded
-                                        : volume! > 0.1
-                                            ? Icons.volume_down_rounded
-                                            : Icons.volume_mute_rounded,
-                                  ),
-                                  Expanded(
-                                    child: Slider(
-                                      value: volume!,
-                                      onChanged: (value) {
-                                        setState(() => volume = value);
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ))
-                          : const SizedBox(),
-                    ],
-                  ),
-                  leading: const Icon(Icons.volume_up),
-                  initialValue: switchValue ?? true,
-                  onToggle: (bool value) {
-                    setState(() {
-                      switchValue = value;
-                    });
-                  },
-                ),
-              ],
-            )
-          ],
-        ));
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ tinnitus_smart_alarm/lib/services/settings_servics.dart
@@ -0,0 +1,55 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsService {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<bool> getLoopAudioSetting() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getBool('loopAlarmAudio') ?? true;
+  }
+
+  Future<void> setLoopAudioSetting(bool value) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setBool('loopAlarmAudio', value);
+  }
+
+  Future<bool> getVibrateSetting() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getBool('vibrate') ?? true;
+  }
+
+  Future<void> setVibrateSetting(bool value) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setBool('vibrate', value);
+  }
+
+  Future<bool> getFadeInSetting() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getBool('fadeIn') ?? true;
+  }
+
+  Future<void> setFadeInSetting(bool value) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setBool('fadeIn', value);
+  }
+
+  Future<bool> getCustomVolumeSetting() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getBool('customVolume') ?? true;
+  }
+
+  Future<void> setCustomVolumeSetting(bool value) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setBool('customVolume', value);
+  }
+
+  Future<double> getVolumeSetting() async {
+    final SharedPreferences prefs = await _prefs;
+    return prefs.getDouble('volume') ?? 1.0;
+  }
+
+  Future<void> setVolumeSetting(double value) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setDouble('volume', value);
+  }
+}
