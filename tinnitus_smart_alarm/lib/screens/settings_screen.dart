b06a53d4c45ea42d73fa48e4e905f3793dd58639
mRJemand <46520059+mRJemand@users.noreply.github.com>
2024-03-24 03:12:10 +0100

    first draft setting screen with pub flutter_settings_ui

--- tinnitus_smart_alarm/lib/screens/settings_screen.dart
+++ tinnitus_smart_alarm/lib/screens/settings_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tinnitus_smart_alarm/widgets/alarm_fav_settings.dart';
+import 'package:flutter_settings_ui/flutter_settings_ui.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -9,13 +9,102 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  bool? switchValue;
+  double? volume = 0.3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: AlarmFavSettings(),
-    );
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
+        body: SettingsList(
+          sections: [
+            SettingsSection(
+              title: const Text('English'),
+              tiles: [
+                SettingsTile(
+                  title: const Text('English'),
+                  leading: const Icon(Icons.language),
+                  onPressed: (BuildContext context) {},
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: const Text('Favorite Alarm Settings'),
+              tiles: [
+                SettingsTile.switchTile(
+                  title: const Text('Loop alarm audio'),
+                  leading: const Icon(Icons.loop),
+                  initialValue: switchValue ?? true,
+                  onToggle: (bool value) {
+                    setState(() {
+                      switchValue = value;
+                    });
+                  },
+                ),
+                SettingsTile.switchTile(
+                  title: const Text('Vibrate'),
+                  leading: const Icon(Icons.vibration),
+                  initialValue: switchValue ?? true,
+                  onToggle: (bool value) {
+                    setState(() {
+                      switchValue = value;
+                    });
+                  },
+                ),
+                SettingsTile.switchTile(
+                  title: const Text('Fade in'),
+                  leading: const Icon(Icons.trending_up_rounded),
+                  initialValue: switchValue ?? true,
+                  onToggle: (bool value) {
+                    setState(() {
+                      switchValue = value;
+                    });
+                  },
+                ),
+                SettingsTile.switchTile(
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Custom volume'),
+                      switchValue ?? false
+                          ? SizedBox(
+                              height: 30,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Icon(
+                                    volume! > 0.7
+                                        ? Icons.volume_up_rounded
+                                        : volume! > 0.1
+                                            ? Icons.volume_down_rounded
+                                            : Icons.volume_mute_rounded,
+                                  ),
+                                  Expanded(
+                                    child: Slider(
+                                      value: volume!,
+                                      onChanged: (value) {
+                                        setState(() => volume = value);
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ))
+                          : const SizedBox(),
+                    ],
+                  ),
+                  leading: const Icon(Icons.volume_up),
+                  initialValue: switchValue ?? true,
+                  onToggle: (bool value) {
+                    setState(() {
+                      switchValue = value;
+                    });
+                  },
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
