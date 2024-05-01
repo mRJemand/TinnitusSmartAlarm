9e755a17eb9b23ed606519d31bf980228be173e7
mRJemand <46520059+mRJemand@users.noreply.github.com>
2024-05-01 16:37:08 +0200

    fix tooltip diagramm

--- tinnitus_smart_alarm/lib/screens/chart_screen.dart
+++ tinnitus_smart_alarm/lib/screens/chart_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tinnitus_smart_alarm/models/survey_result.dart';
+import 'package:tinnitus_smart_alarm/services/firestore_manager.dart';
 import 'package:tinnitus_smart_alarm/widgets/survey_data_chart.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ChartScreen extends StatelessWidget {
+class ChartScreen extends StatefulWidget {
   const ChartScreen({super.key});
+
+  @override
+  State<ChartScreen> createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  List<SurveyResult> surveyResults = [];
+
+  FirestoreManager firestoreManager = FirestoreManager();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    var results = await firestoreManager.fetchDataFromFirestore();
+    setState(() {
+      surveyResults = results;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +45,9 @@ class ChartScreen extends StatelessWidget {
             ),
             SizedBox(
               height: 400,
-              child: SurveyDataChart(),
+              child: SurveyDataChart(
+                surveyResults: surveyResults,
+              ),
             ),
           ],
         ),
--- tinnitus_smart_alarm/lib/widgets/survey_data_chart.dart
+++ tinnitus_smart_alarm/lib/widgets/survey_data_chart.dart
@@ -1,106 +1,162 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
 import 'package:tinnitus_smart_alarm/models/survey_result.dart';
-import 'package:tinnitus_smart_alarm/services/firestore_manager.dart';
 
 class SurveyDataChart extends StatefulWidget {
+  final List<SurveyResult> surveyResults;
+
+  SurveyDataChart({Key? key, required this.surveyResults}) : super(key: key);
+
   @override
   _SurveyDataChartState createState() => _SurveyDataChartState();
 }
 
 class _SurveyDataChartState extends State<SurveyDataChart> {
-  List<SurveyResult> surveyResults = [];
-  FirestoreManager firestoreManager = FirestoreManager();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadData();
-  }
-
-  Future<void> _loadData() async {
-    var results = await firestoreManager.fetchDataFromFirestore();
-    setState(() {
-      surveyResults = results;
-    });
-  }
+  SurveyResult? selectedResult;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: LineChart(
-          LineChartData(
-            minY: 0,
-            maxY: 10,
-            gridData: const FlGridData(show: true),
-            titlesData: FlTitlesData(
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
+    return Column(
+      children: [
+        Expanded(
+          child: LineChart(
+            LineChartData(
+              minX: 0,
+              maxX: widget.surveyResults.length.toDouble(),
+              minY: 0,
+              maxY: 10,
+              titlesData: FlTitlesData(
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
                     showTitles: true,
-                    reservedSize: 40,
                     getTitlesWidget: (double value, TitleMeta meta) {
-                      return Text("Day ${value.toInt()}",
+                      final index = value.toInt();
+                      if (index < widget.surveyResults.length) {
+                        return Text(
+                          DateFormat('dd-MM')
+                              .format(widget.surveyResults[index].dateTime),
                           style: const TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold));
-                    }),
-              ),
-              leftTitles: AxisTitles(
-                sideTitles: SideTitles(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        );
+                      }
+                      return const Text('');
+                    },
+                  ),
+                ),
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
                     showTitles: true,
-                    reservedSize: 40,
-                    getTitlesWidget: (double value, TitleMeta meta) {
-                      return Text("${value.toInt()}",
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold));
-                    }),
+                    getTitlesWidget: (double value, TitleMeta meta) =>
+                        Text(value.toInt().toString()),
+                  ),
+                ),
               ),
-              topTitles:
-                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              rightTitles:
-                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              gridData: FlGridData(show: true),
+              borderData: FlBorderData(show: true),
+              lineBarsData: [
+                LineChartBarData(
+                  spots: widget.surveyResults
+                      .asMap()
+                      .entries
+                      .map((entry) => FlSpot(
+                          entry.key.toDouble(), entry.value.intenseResult))
+                      .toList(),
+                  isCurved: false,
+                  color: Colors.red,
+                  barWidth: 5,
+                  isStrokeCapRound: true,
+                  dotData: FlDotData(show: true),
+                  // belowBarData: BarAreaData(show: true),
+                ),
+                LineChartBarData(
+                  spots: widget.surveyResults
+                      .asMap()
+                      .entries
+                      .map((entry) =>
+                          FlSpot(entry.key.toDouble(), entry.value.ignorable))
+                      .toList(),
+                  isCurved: false,
+                  color: Colors.blue,
+                  barWidth: 5,
+                  isStrokeCapRound: true,
+                  dotData: FlDotData(show: true),
+                  // belowBarData: BarAreaData(show: true),
+                ),
+                LineChartBarData(
+                  spots: widget.surveyResults
+                      .asMap()
+                      .entries
+                      .map((entry) => FlSpot(
+                          entry.key.toDouble(), entry.value.uncomfortable))
+                      .toList(),
+                  isCurved: false,
+                  color: Colors.green,
+                  barWidth: 5,
+                  isStrokeCapRound: true,
+                  dotData: FlDotData(show: true),
+                  // belowBarData: BarAreaData(show: true),
+                ),
+              ],
+              lineTouchData: LineTouchData(
+                touchCallback:
+                    (FlTouchEvent event, LineTouchResponse? touchResponse) {
+                  if (event is FlTapUpEvent && touchResponse != null) {
+                    final index = touchResponse.lineBarSpots?.first.x.toInt();
+                    if (index != null &&
+                        index >= 0 &&
+                        index < widget.surveyResults.length) {
+                      setState(() {
+                        selectedResult = widget.surveyResults[index];
+                      });
+                    }
+                  }
+                },
+                handleBuiltInTouches: true,
+                touchTooltipData: LineTouchTooltipData(
+                  getTooltipItems: (List<LineBarSpot> touchedSpots) {
+                    return touchedSpots.map((LineBarSpot spot) {
+                      String tooltipText = '';
+                      if (spot.barIndex == 0) {
+                        tooltipText = 'Intense: ${spot.y.toStringAsFixed(2)}';
+                      } else if (spot.barIndex == 1) {
+                        tooltipText = 'Ignorable: ${spot.y.toStringAsFixed(2)}';
+                      } else if (spot.barIndex == 2) {
+                        tooltipText =
+                            'Uncomfortable: ${spot.y.toStringAsFixed(2)}';
+                      }
+                      return LineTooltipItem(
+                        tooltipText,
+                        TextStyle(color: Colors.white),
+                      );
+                    }).toList();
+                  },
+                  fitInsideHorizontally: true,
+                  fitInsideVertically: true,
+                ),
+              ),
             ),
-            borderData: FlBorderData(show: true),
-            lineBarsData: [
-              lineChartBarData('Intensität', Colors.red),
-              lineChartBarData('Unbehagen', Colors.green),
-              lineChartBarData('Ignorierbarkeit', Colors.blue),
-            ],
-            lineTouchData: const LineTouchData(enabled: true),
           ),
         ),
-      ),
-    );
-  }
-
-  LineChartBarData lineChartBarData(String label, Color color) {
-    List<double> results = surveyResults.map((e) {
-      switch (label) {
-        case 'Intensität':
-          return e.intenseResult;
-        case 'Unbehagen':
-          return e.uncomfortable;
-        case 'Ignorierbarkeit':
-          return e.ignorable;
-        default:
-          return 0.0; // Should not happen
-      }
-    }).toList();
-
-    return LineChartBarData(
-      spots: [
-        for (int i = 0; i < results.length; i++)
-          FlSpot(i.toDouble(), results[i]),
+        if (selectedResult != null)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Zeit: ${DateFormat('dd-MM-yyyy HH:mm').format(selectedResult!.dateTime)}\n'
+              'Intense: ${selectedResult!.intenseResult.toStringAsFixed(2)}\n'
+              'Ignorable: ${selectedResult!.ignorable.toStringAsFixed(2)}\n'
+              'Uncomfortable: ${selectedResult!.uncomfortable.toStringAsFixed(2)}\n'
+              'Stimuli: ${selectedResult!.stimuli}\n'
+              'Frequency: ${selectedResult!.frequency}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
       ],
-      color: color,
-      barWidth: 2,
-      isStrokeCapRound: true,
-      dotData: const FlDotData(show: true),
-      belowBarData: BarAreaData(show: false),
     );
   }
 }
