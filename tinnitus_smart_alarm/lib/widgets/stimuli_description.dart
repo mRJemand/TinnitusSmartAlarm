f0bab1796c030251a1810fe0de5ef28b3eaeb2cd
mRJemand <46520059+mRJemand@users.noreply.github.com>
2024-05-05 18:04:17 +0200

    translate stimuli description

--- tinnitus_smart_alarm/lib/widgets/stimuli_description.dart
+++ tinnitus_smart_alarm/lib/widgets/stimuli_description.dart
@@ -1,77 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StimuliDescriptionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Stimuli Erklärung'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Erklärung verwendete Stimuli / Auditorische Stimuli für Tinnitus-Behandlung DE',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.stimuliDescriptionTitle,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              AppLocalizations.of(context)!.stimuliDescription1,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            _buildSection(
+              title: AppLocalizations.of(context)!.naturalPlus,
+              content:
+                  AppLocalizations.of(context)!.stimuliDescriptionPartNatSpec,
+              imagePath: 'assets/images/natuerlich_spezifisch.png',
+            ),
+            _buildSection(
+              title: AppLocalizations.of(context)!.naturalNeg,
+              content: AppLocalizations.of(context)!
+                  .stimuliDescriptionPartNatNotSpec,
+              imagePath: 'assets/images/natuerlich_nicht_spezifisch.png',
+            ),
+            _buildSection(
+              title: AppLocalizations.of(context)!.unnaturalPos,
+              content: AppLocalizations.of(context)!
+                  .stimuliDescriptionPartDigitalSpec,
+              imagePath: 'assets/images/digital_spezifisch.png',
+            ),
+            _buildSection(
+              title: AppLocalizations.of(context)!.unnaturalNeg,
+              content: AppLocalizations.of(context)!
+                  .stimuliDescriptionPartDigitalNotSpec,
+              imagePath: 'assets/images/digital_nicht_spezifisch.png',
+            ),
+            SizedBox(height: 16),
+            Text(
+              AppLocalizations.of(context)!
+                  .stimuliDescriptionAdditionalConsiderationsTitle,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              AppLocalizations.of(context)!
+                  .stimuliDescriptionAdditionalConsiderationsText,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              AppLocalizations.of(context)!
+                  .stimuliDescriptionPersonalizationTitle,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              AppLocalizations.of(context)!
+                  .stimuliDescriptionPersonalizationText,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.close_outlined),
+                label: Text(AppLocalizations.of(context)!.close),
+                onPressed: () => Navigator.pop(context),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
                 ),
               ),
-              SizedBox(height: 16),
-              Text(
-                'Die App beinhaltet 64 unterschiedlich konzipierte auditorische Stimuli, die in vier spezifische Kategorien gegliedert sind. Jede Kategorie beinhaltet 16 Sounds, die sich in Herkunft (natürlich oder digital) und Bezug zu Tinnitus (spezifisch oder nicht-spezifisch) unterscheiden:',
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 8),
-              BulletList([
-                'Natürlich und spezifisch',
-                'Natürlich und nicht-spezifisch',
-                'Digital und spezifisch',
-                'Digital und nicht-spezifisch',
-              ]),
-              SizedBox(height: 16),
-              _buildSection(
-                title: '1. Natürlich und spezifisch',
-                content:
-                    'Diese Kategorie umfasst natürliche Geräusche, die speziell auf eine Tinnitusfrequenz von 4 kHz abgestimmt sind, um die Wahrnehmung von Tinnitus gezielt zu beeinflussen. Beispiele hierfür sind speziell modulierte Rauschgeräusche, die helfen können, Tinnitus wahrnehmbar zu maskieren.',
-                imagePath: 'assets/images/natuerlich_spezifisch.png',
-              ),
-              _buildSection(
-                title: '2. Natürlich und nicht-spezifisch',
-                content:
-                    'Diese Stimuli bestehen aus natürlichen Geräuschen, die keine direkte Anpassung auf die Tinnitusfrequenz aufweisen. Sie variieren in ihrer spektralen Komplexität und temporalen Modulation, wie etwa das Rauschen von Wasserfällen oder Meereswellen, die allgemein beruhigende Effekte haben, jedoch nicht spezifisch auf Tinnitus abzielen.',
-                imagePath: 'assets/images/natuerlich_nicht_spezifisch.png',
-              ),
-              _buildSection(
-                title: '3. Digital und spezifisch',
-                content:
-                    'In dieser Kategorie sind digitale Sounds enthalten, die auf die Tinnitusfrequenz von 4 kHz ausgerichtet sind. Diese können durch Frequenz- und Amplitudenmodulationen gestaltet sein, um das tinnitusbezogene Hörerlebnis zu beeinflussen, beispielsweise durch Erzeugung eines Piepens oder eines pulsierenden Rauschens.',
-                imagePath: 'assets/images/digital_spezifisch.png',
-              ),
-              _buildSection(
-                title: '4. Digital und nicht-spezifisch',
-                content:
-                    'Diese Gruppe umfasst digitale Geräusche, die keine spezifische Ausrichtung auf die Tinnitusfrequenz haben. Sie bieten eine Vielfalt an Klängen, die für eine breitere auditive Stimulation sorgen, wie tiefere Töne und ungefiltertes digitales Rauschen, welche die Wahrnehmung von Tinnitus durch unterschiedliche akustische Umgebungen beeinflussen können.',
-                imagePath: 'assets/images/digital_nicht_spezifisch.png',
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Zusätzliche Überlegungen:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Die Stimuli innerhalb jeder Kategorie wurden sorgfältig nach spektraler Komplexität und temporaler Modulation ausgewählt, um die auditive Erfahrung zu maximieren. Beispiele hierfür sind das einfache Pfeifen eines Wasserkessels oder das komplexe Rauschen einer Dusche, die je nach Stimulus von minimaler bis maximaler Modulation variieren können. Diese sorgfältige Auswahl hilft, die Effektivität der Tinnitusbehandlung durch akustische Stimulation zu maximieren.',
-                style: TextStyle(fontSize: 16),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
